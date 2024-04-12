3200a8f6b13ba2c79c958352be3619065faf233a
maneesh29s <sutarmaneesh00@gmail.com>
2024-04-12 18:17:37 +0530

    Update compile flags for clangd. Update timer.

--- gists/timer.cpp
+++ gists/timer.cpp
@@ -1,17 +1,33 @@
+#include <chrono>
+#include <string>
+
 template <typename T>
 class Timer {
-private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> start;
-  std::chrono::time_point<std::chrono::high_resolution_clock> end;
+   private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    std::chrono::time_point<std::chrono::high_resolution_clock> end;
 
-public:
-  void start_timer() { this->start = std::chrono::high_resolution_clock::now(); }
-  void stop_timer() { this->end = std::chrono::high_resolution_clock::now(); }
+   public:
+    void start_timer() {
+        this->start = std::chrono::high_resolution_clock::now();
+    }
+    void stop_timer() {
+        this->end = std::chrono::high_resolution_clock::now();
+    }
 
-  std::string time_elapsed() {
-    auto time_taken =
-        std::chrono::duration_cast<T>(this->end - this->start).count();
+    std::string time_elapsed() {
+        auto time_taken = std::chrono::duration_cast<T>(this->end - this->start).count();
 
-    return std::to_string(time_taken);
-  }
+        std::string unit = "";
+        if (std::is_same<T, std::chrono::seconds>::value)
+            unit = "s";
+        else if (std::is_same<T, std::chrono::milliseconds>::value)
+            unit = "ms";
+        else if (std::is_same<T, std::chrono::microseconds>::value)
+            unit = "us";
+        else if (std::is_same<T, std::chrono::nanoseconds>::value)
+            unit = "ns";
+
+        return std::to_string(time_taken) + " " + unit;
+    }
 };
